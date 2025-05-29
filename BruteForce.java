87225d9477790c1141476e1ccecd0e621bf2834c
Joshua Liu <liu.joshua555@gmail.com>
2025-05-28 22:58:20 -0400

    created a dockerized CLI version of the original JavaFX GUI

--- BruteForce.java
+++ BruteForce.java
@@ -3,13 +3,6 @@ import java.util.Scanner;
 public class BruteForce {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-            sum += arr[i];
-        }
-        System.out.println(sum);
+
     }
 }
--- Generator.java
+++ Generator.java
@@ -2,12 +2,6 @@ import java.util.Random;
 
 public class Generator {
     public static void main(String[] args) {
-        Random random = new Random();
-        int n = random.nextInt(10) + 1;
-        System.out.println(n);
-        for (int i = 0; i < n; i++) {
-            int num = random.nextInt(5) + 1;
-            System.out.print(num + " ");
-        }
+
     }
 }
